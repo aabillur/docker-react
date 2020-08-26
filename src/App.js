c680c4d67fe261787acfbfb3e03fcf5332d89617
Arun appasaheb Billur <arunbillur@xiaomi.com>
2020-08-26 19:15:48 +0530

    docker react

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Arun billur14
+          Arun
         </p>
         <a
           className="App-link"
